53abb3378766b1b98116a94ce214956686c927a2
MITSUNARI Shigeo <herumi@nifty.com>
2014-12-16 00:09:08 +0900

    update makefile

--- common.mk
+++ common.mk
@@ -1,40 +1,54 @@
-GCC_VER=$(shell gcc -dumpversion)
-ifeq ($(shell expr $(GCC_VER) \> 4.2.1),1)
-  CFLAGS_OPT +=-march=native
+GCC_VER=$(shell $(PRE)$(CC) -dumpversion)
+UNAME_S=$(shell uname -s)
+ifeq ($(UNAME_S),Linux)
+  OS=Linux
 endif
-ifeq ($(CXX),clang++)
-  CFLAGS_OPT += -O3
+ifeq ($(UNAME_S),Darwin)
+  CFLAGS+=-std=c++11 # clang
 else
-  ifeq ($(shell expr $(GCC_VER) \> 4.6.0),1)
-    CFLAGS_OPT +=-Ofast
-  else
-    CFLAGS_OPT += -O3
-  endif
-endif
-BIT=64
-ifeq ($(shell uname -o),Cygwin)
-  BIT=32
+  LDFLAGS += -lrt
 endif
 CP = cp -f
 AR = ar r
 MKDIR=mkdir -p
 RM=rm -fr
-CFLAGS_OPT += -fomit-frame-pointer -DNDEBUG
-CFLAGS_WARN = -Wall -Wextra -Wformat=2 -Wcast-qual -Wcast-align -Wwrite-strings -Wfloat-equal -Wpointer-arith #-Wswitch-enum -Wstrict-aliasing=2
-CFLAGS += -g -D_FILE_OFFSET_BITS=64 -msse2
-CFLAGS += $(CFLAGS_WARN)
-LDFLAGS += -lpthread -lssl -lcrypto -lgmp -lgmpxx #-ltcmalloc_minimal
-ifneq ($(shell uname -s),Darwin)
-  LDFLAGS += -lrt
+CFLAGS_OPT+=-fomit-frame-pointer -DNDEBUG
+ifeq ($(CXX),clang++)
+  CFLAGS_OPT+=-O3
+else
+  ifeq ($(shell expr $(GCC_VER) \> 4.6.0),1)
+    CFLAGS_OPT+=-Ofast
+  else
+    CFLAGS_OPT+=-O3
+  endif
 endif
-ifeq ($(USE_TCMALLOC),1)
-  CFLAGS += -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free
-  LDFLAGS += -ltcmalloc_minimal
+CFLAGS_WARN=-Wall -Wextra -Wformat=2 -Wcast-qual -Wcast-align -Wwrite-strings -Wfloat-equal -Wpointer-arith
+CFLAGS+= -g -D_FILE_OFFSET_BITS=64
+CFLAGS+=$(CFLAGS_WARN)
+BIT?=64
+ifeq ($(BIT),32)
+  CPU?=x86
+else
+  ifeq ($(BIT),64)
+    CPU?=x64
+  endif
+endif
+ifeq ($(BIT),0)
+	BIT_OPT=
+else
+	BIT_OPT=-m$(BIT)
+endif
+ifeq ($(MARCH),)
+ifeq ($(shell expr $(GCC_VER) \> 4.2.1),1)
+	CFLAGS+=-march=native
+endif
+else
+	CFLAGS+=$(MARCH)
 endif
 
 DEBUG=1
 ifeq ($(RELEASE),1)
-  DEBUG=0
+	DEBUG=0
 endif
 
 ifeq ($(DEBUG),0)
@@ -42,7 +56,7 @@ ifeq ($(DEBUG),0)
   OBJDIR=release
   OBJSUF=
 else
-  ifeq ($(shell uname -s),Linux)
+  ifeq ($(OS),Linux)
     LDFLAGS+=-rdynamic
   endif
   OBJDIR=debug
@@ -51,10 +65,21 @@ endif
 
 TOPDIR:=$(realpath $(dir $(lastword $(MAKEFILE_LIST))))/
 EXTDIR:=$(TOPDIR)../cybozulib_ext/
-CFLAGS+= -m$(BIT) -I$(TOPDIR)include  -I$(TOPDIR)../cybozulib/include/ -I$(TOPDIR)../mie/include -I$(TOPDIR)../xbyak/
-LDFLAGS+= -m$(BIT)
+CFLAGS+= -I$(TOPDIR)include -I$(TOPDIR)../cybozulib/include/ -I$(TOPDIR)../xbyak/ $(BIT_OPT) $(INC_DIR)
+LDFLAGS+= -L$(TOPDIR)lib $(BIT_OPT) -Wl,-rpath,'$$ORIGIN/../lib' $(LD_DIR)
 
-MKDEP = sh -ec '$(CC) -MM $(CFLAGS) $< | sed "s@\($*\)\.o[ :]*@$(OBJDIR)/\1.o $@ : @g" > $@; [ -s $@ ] || rm -f $@'
+####################################################
+
+CFLAGS+=-I$(TOPDIR)../mie/include
+
+ifeq ($(USE_TCMALLOC),1)
+  CFLAGS += -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free
+  LDFLAGS += -ltcmalloc_minimal
+endif
+
+####################################################
+
+MKDEP = sh -ec '$(PRE)$(CC) -MM $(CFLAGS) $< | sed "s@\($*\)\.o[ :]*@$(OBJDIR)/\1.o $@ : @g" > $@; [ -s $@ ] || rm -f $@; touch $@'
 
 CLEAN=$(RM) $(TARGET) $(OBJDIR)
 
@@ -70,13 +95,13 @@ endef
 .SUFFIXES: .cpp .d .exe
 
 $(OBJDIR)/%.o: %.cpp
-	$(CXX) -c $< -o $@ $(CFLAGS)
+	$(PRE)$(CXX) -c $< -o $@ $(CFLAGS)
 
 $(OBJDIR)/%.d: %.cpp $(OBJDIR)
 	@$(MKDEP)
 
 $(TOPDIR)bin/%$(OBJSUF).exe: $(OBJDIR)/%.o $(LIBS)
-	$(CXX) $< -o $@ $(LIBS) $(LDFLAGS)
+	$(PRE)$(CXX) $< -o $@ $(LIBS) $(LDFLAGS)
 
 OBJS=$(addprefix $(OBJDIR)/,$(SRC:.cpp=.o))
 
